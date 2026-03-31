69ab56fb4df89261112096f2402b4d36d40b2d8a
uditpal518-coder <uditpal518@gmail.com>
2026-03-31 14:31:31 +0530

    Update Sentiment_Project.py

--- Sentiment_Project.py
+++ Sentiment_Project.py
@@ -4,7 +4,7 @@ import re
 import pandas as pd
 import numpy as np
 
-st.set_page_config(layout='wide', page_title="Food Sentiment Analysis", page_icon="🇮🇳")
+st.set_page_config(layout='wide', page_title="Food Sentiment Analysis", page_icon="🍔")
 
 st.markdown("""
     <style>
